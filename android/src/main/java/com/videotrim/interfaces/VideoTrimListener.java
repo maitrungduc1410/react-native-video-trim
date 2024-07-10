29e4c42c8b3565b9563b28d8930a2791e6e01417
Terence O'Donoghue <terence@weareaduro.com>
2024-07-10 10:59:31 +0100

    Update VideoTrimListener.java
    Added params to interface

--- android/src/main/java/com/videotrim/interfaces/VideoTrimListener.java
+++ android/src/main/java/com/videotrim/interfaces/VideoTrimListener.java
@@ -3,7 +3,7 @@ package com.videotrim.interfaces;
 public interface VideoTrimListener {
   void onStartTrim();
   void onTrimmingProgress(int percentage);
-  void onFinishTrim(String url);
+  void onFinishTrim(String url, long startMs, long endMs, int videoDuration);
   void onError(String errorMessage);
   void onCancel();
   void onSave();
